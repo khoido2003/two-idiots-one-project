43c82a467f13bd53987d9ad0449a7759a5fbc78f
LeeDat03 <leedat104517@gmail.com>
2025-03-24 00:10:33 +0700

    Update config prettier

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script>
-    import "../app.css";
-    import Navbar from "$lib/components/Navbar.svelte";
-
+    import '../app.css';
+    import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<Navbar/>
-<slot/>
-
+<Navbar />
+<slot />
